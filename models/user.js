e0a47ecd3fecd52656c30806af622a367530c1bd
Vera Chashchina <chaschinavera@gmail.com>
2023-07-06 09:06:58 +0300

    Исправить баги

--- models/user.js
+++ models/user.js
@@ -7,23 +7,19 @@ const userSchema = new mongoose.Schema({
     default: 'Жак-Ив Кусто',
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     default: 'Исследователь',
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (v) => validator.isURL(v),
       message: 'Некорректный URL',
     },
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
